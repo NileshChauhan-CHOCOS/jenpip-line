d814c926ec37a785a374e6d1fae39aadcadedd9c
Nilesh Chauhan <nileshchauhannile@gmail.com>
2025-04-26 17:39:36 +0530

    repository created
    Signed-off-by: Nilesh Chauhan <nileshchauhannile@gmail.com>

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+node {
+    agent any
+    stages {
+
+        stage('checkout') {
+            steps {
+                echo "This is checkout step"
+            }
+        }
+        stage('Build') {
+            steps {
+                echo "This is build step"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "This is test step"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "Successfully deployed Thank you !!!"
+            }
+        }
+    }
+}

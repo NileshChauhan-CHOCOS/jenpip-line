b6b23691396df92c12c90893fc89e17dac195a34
Nilesh Chauhan <nileshchauhannile@gmail.com>
2025-05-01 20:09:33 +0530

    checkout script added
    Signed-off-by: Nilesh Chauhan <nileshchauhannile@gmail.com>

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,13 @@
 pipeline {
     agent any
     stages {
-
-        stage('checkout') {
+        stage('Git Checkout') {
             steps {
+                script{
+                    git branch: 'main',
+                    credentialsId : "4328481",
+                    url : 'git@github.com:NileshChauhan-CHOCOS/doc-cicd.git'
+                }
                 echo "This is checkout step"
             }
         }
